e9fa58e0d2ab8e97e02356161d028ff7e5a05550
Christopher Moura <74259597+ChrisMoura2000@users.noreply.github.com>
2022-11-16 15:17:41 -0300

    first commit

---
+++ Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿namespace Crud7.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefone { get; set; }
+        
+        public int Idade { get; set; }
+    }
+}
